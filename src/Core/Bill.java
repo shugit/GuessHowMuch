c4561f23c9c2edd3dbdfc5a791924a1ab9572456
Sephy Zhou <jacobc3@gmail.com>
2014-06-25 17:23:13 +0800

    initialize

---
+++ src/Core/Bill.java
@@ -0,0 +1,36 @@
+package Core;
+
+import java.awt.Image;
+
+public class Bill implements Object {
+	String name;
+	int minValue;
+	int maxValue;
+	Image image;
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int minValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int maxValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Core/Object.java
@@ -0,0 +1,7 @@
+package Core;
+
+public interface Object {
+	public String getName();
+	public int minValue();
+	public int maxValue();
+}
